0ac2adae58a5a8b9f7991caaa65d1c74358b91a4
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-11 14:25:48 -0600

    figured out addnode method, now working on search

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -20,8 +20,39 @@ namespace BinarySearchTree
         //member methods
         public void AddNode(int num)
         {
-
+            Node newNode = new Node();
+            newNode.data = num;
+            if(root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if(num < current.data)
+                    {
+                        current = current.left;
+                        if(current == null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                        else
+                        {
+                            current = current.right;
+                            if(current == null)
+                            {
+                                parent.right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
         }
-
     }
 }
