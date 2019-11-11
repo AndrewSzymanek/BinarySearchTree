b2e425145b9ef6d36ff268a89fd28ea8ff854918
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-11 13:55:38 -0600

    got classes set up with member variables, starting to add method for adding a node

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        //member variables
+        public Node root;
+
+        //constructor
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        //member methods
+        public void AddNode(int num)
+        {
+
+        }
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        //member variables
+
+        public int data;
+        public Node left;
+        public Node right;
+
+        //constructor
+        public Node()
+        {
+
+        }
+
+        //member methods
+        public void DisplayNode()
+        {
+            Console.Write(data + " ");
+        }
+    }
+}
